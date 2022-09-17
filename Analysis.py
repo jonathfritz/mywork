ac47ec372a030c622c9e84d7764bf124e9f8f170
jonathfritz <jonathanfritz@gmx.net>
2022-09-17 16:55:36 +0200

    Importing CsV-File

--- Analysis.py
+++ Analysis.py
@@ -1,5 +1,8 @@
-import csv
-
-with open('')
+import csv;
 
 
+with open('Quellen.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+
+    for columns in csv_reader:
+        print(columns[3])
