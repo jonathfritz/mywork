f413eeec9b95a8c9e5defda62b2d9ef8745707fd
jonathfritz <jonathanfritz@gmx.net>
2022-09-18 02:18:09 +0200

    Improving analysis code

--- Analysis.py
+++ Analysis.py
@@ -1,8 +1,17 @@
 import csv;
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-with open('Quellen.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
+#with open('Quellen.csv', 'r') as csv_file:
+  #  csv_reader = csv.reader(csv_file, delimiter=';')
+#
+  #  for columns in csv_reader:
+   #     print(columns[0])
 
-    for columns in csv_reader:
-        print(columns[3])
+df = pd.read_csv('C:\Users\jonat\OneDrive\Dokumente\GitHub\mywork\Quellen.csv')
+print(df.shape)
+
+# gut
