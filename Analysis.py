60bd650ecdaf3122df1339be171c7540dfde0350
jonathfritz <jonathanfritz@gmx.net>
2022-09-21 23:18:52 +0200

    Adding way to clean text

--- Analysis.py
+++ Analysis.py
@@ -4,10 +4,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import re
+import string
+
+import nltk
+
+
+# Step 1: Read in Data
+
+
+
+def clean_text(text):
+    # Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.'''
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
 
 with open('Quellen.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
-
     for columns in csv_reader:
+        # defining GEFEG as the analyzed target
         if columns[0] == 'GEFEG':
-            print(columns[3])
+            print(clean_text(columns[3]))
+
+
+
+
