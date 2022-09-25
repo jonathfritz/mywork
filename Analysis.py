f58b15afb8fb0c5c8c58a40b4795f048828ea501
jonathan <jonathanfritz@gmx.net>
2022-09-25 17:46:39 +0200

    end of sunday state

--- Analysis.py
+++ Analysis.py
@@ -1,13 +1,12 @@
 import csv;
 import pandas as pd
 import numpy as np
-import textstat
 import re
 import string
 import textstat
 import nltk
 from transformers import pipeline
-
+import pprint
 
 # Step 1: Read in Data
 
@@ -38,11 +37,21 @@ with open('Quellen.csv', 'r') as csv_file:
             GEFEG_cleaned_text = clean_text(columns[3])
             GEFEF_normal_text = columns[3]
 
+# Die beiden Textkorpusse werden jeweils seperat auf ihre Lesefreundlichkeit gemessen.
+# Da die Satzzeichen bei diesem Test eine wichtige Rolle spielen, wurden für diesen Test die orginalen Textpassagen ohne Überabrietung genutzt.
+print('GEFEG Flesh Reading Score: ' + str(textstat.flesch_reading_ease(GEFEF_normal_text)))
+print('Ecosio Flesh Reading Score: ' + str(textstat.flesch_reading_ease(Ecosio_normal_text)))
 
-print('GEFEG Flesh Reading Score: ' + textstat.flesch_reading_ease(GEFEF_normal_text))
-print('Ecosio Flesh Reading Score: ' + textstat.flesch_reading_ease(Ecosio_normal_text))
 
 
-sentiment_pipeline = pipeline("sentiment-analysis")
+words = [w for w in nltk.corpus.state_union.words() if w.isalpha()]
+stopwords = nltk.corpus.stopwords.words("english")
+#Wörter ohne Stoppwörter (aber alle in lower case)
+words = [w for w in words if w.lower() not in stopwords]
+
+# use str.isalpha to filter out punctuation
+pprint((nltk.word_tokenize(GEFEG_cleaned_text), width=79, compact=True)
+
+#sentiment_pipeline = pipeline("sentiment-analysis")
 #data = ["I love you", "I hate you"]
 #sentiment_pipeline(data)
