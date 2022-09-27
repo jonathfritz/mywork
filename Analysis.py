8c4e6e0f0beb8965d13914a65155a301896a81ea
jonathan <jonathanfritz@gmx.net>
2022-09-27 14:21:53 +0200

    a lot of improvements

--- Analysis.py
+++ Analysis.py
@@ -5,20 +5,39 @@ import re
 import string
 import textstat
 import nltk
-from transformers import pipeline
-import pprint
-
-# Step 1: Read in Data
-
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 
 def clean_text(text):
     # Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.'''
     text = text.lower()
-    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('\[.*?]', '', text)
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\w*\d\w*', '', text)
     return text
+
+# def tokenize_text(text):
+#     stopwords = nltk.corpus.stopwords.words("english")
+#     words = [w for w in words if w.lower() not in stopwords]
+#     words: list[str] = nltk.word_tokenize(text)
+#     frequent_words = nltk.FreqDist(words)
+#     lower_frequent_words = nltk.FreqDist([w.lower() for w in frequent_words])
+#     stopwords = nltk.corpus.stopwords.words("english")
+#     filtered_words = [t for t in lower_frequent_words if not t in stopwords.words("english")]
+#     return filtered_words.most_common(10)
+
+def tokenize_text(text):
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    filtered_text = [w for w in word_tokens if not w.lower() in stop_words]
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_text.append(w)
+            frequent_words = nltk.FreqDist(filtered_text)
+            return frequent_words.most_common(5)
+
+
 
 # die Artikel und Textpassagen aus dem Excel-File werden in das Programm hochgeladen
 with open('Quellen.csv', 'r') as csv_file:
@@ -38,28 +57,80 @@ with open('Quellen.csv', 'r') as csv_file:
             GEFEF_normal_text = columns[3]
 
 
-
 with open("Quellen.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
     skip_header = True
-    i = 0;
+    i = 0
     for row in csv_reader:
         if skip_header:
             skip_header = False
             continue
         if row[0] == "ECOSIO":
-            print('Ecosio Flesh Reading Score: ' + str(textstat.flesch_reading_ease(row[3])))
+            result = tokenize_text(clean_text(row[3]))
+            print('Ecosio, ' + str(row[2]) + ': ' + str(result))
         if row[0] == "GEFEG":
-            print('GEFEG Flesh Reading Score: ' + str(textstat.flesch_reading_ease(row[3])))
-
+            result= tokenize_text(clean_text(row[3]))
+            print('GEFEG, ' + str(row[2]) + ': ' + str(result))
         i += 1
 
 
-# Die beiden Textkorpusse werden jeweils seperat auf ihre Lesefreundlichkeit gemessen.
-# Da die Satzzeichen bei diesem Test eine wichtige Rolle spielen, wurden für diesen Test die orginalen Textpassagen ohne Überabrietung genutzt.
-#print('GEFEG Flesh Reading Score: ' + str(textstat.flesch_reading_ease(GEFEF_normal_text)))
-#print('Ecosio Flesh Reading Score: ' + str(textstat.flesch_reading_ease(Ecosio_normal_text)))
 
+
+
+
+
+
+
+
+
+# Flesch_reading_ease_score Completed
+# with open("Quellen.csv", "r") as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=";")
+#     skip_header = True
+#     i = 0;
+#     for row in csv_reader:
+#         if skip_header:
+#             skip_header = False
+#             continue
+#         if row[0] == "ECOSIO":
+#             print('Ecosio Flesh Reading Score: ' + str(row[2]) + ': ' + str(textstat.flesch_reading_ease(row[3])))
+#         if row[0] == "GEFEG":
+#             print('GEFEG Flesh Reading Score: ' + str(row[2]) + ': ' + str(textstat.flesch_reading_ease(row[3])))
+#
+#         i += 1
+
+
+
+
+
+
+# with open("Quellen.csv", "r") as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=";")
+#     skip_header = True
+#     i = 0
+#     for row in csv_reader:
+#         if skip_header:
+#             skip_header = False
+#             continue
+#         if row[0] == "ECOSIO":
+#             #(pprint(nltk.word_tokenize(row[3]), width=79, compact=True))
+#             Ecosio_words: list= nltk.word_tokenize(row[3])
+#             frequent_Ecosio_words = nltk.FreqDist(Ecosio_words)
+#             lower_frequent_Ecosio_words = nltk.FreqDist([w.lower() for w in frequent_Ecosio_words])
+#             print(lower_frequent_Ecosio_words.most_common(10))
+#             words = [w for w in nltk.corpus.state_union.words() if w.isalpha()]
+#             stopwords = nltk.corpus.stopwords.words("english")
+#             words = [w for w in words if w.lower() not in stopwords]
+#         if row[0] == "GEFEG":
+#             # pprint(nltk.word_tokenize(row[3]), width=79, compact=True)
+#             GEFEG_words: list = nltk.word_tokenize(row[3])
+#             frequent_GEFEG_words = nltk.FreqDist(GEFEG_words)
+#             lower_frequent_GEFEG_words = nltk.FreqDist([w.lower() for w in frequent_GEFEG_words])
+#             lower_frequent_GEFEG_words.most_common(10)
+#             words = [w for w in nltk.corpus.state_union.words() if w.isalpha()]
+#             stopwords = nltk.corpus.stopwords.words("english")
+#             words = [w for w in words if w.lower() not in stopwords]
+#         i += 1
 
 
 words = [w for w in nltk.corpus.state_union.words() if w.isalpha()]
