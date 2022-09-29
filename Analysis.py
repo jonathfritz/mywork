4958a871b6d3d00cd81731865fb753015b30a718
jonathfritz <jonathanfritz@gmx.net>
2022-09-29 20:41:15 +0200

    Finalisierung

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,4 @@
+"""Anbei sind die Libaries aufgelistet, die für die genutzten Methoden benutzt wurden."""
 import csv
 import re
 import string
@@ -7,21 +8,21 @@ from nltk.tokenize import word_tokenize
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import textstat
 
-
-
-
+"""Diese Funktion reduziert die Komplexität der Textpassagen.
+Sie entfernt Satzzeichen, setzt bei jedem Wort die Kleinschreibung
+durch und entfernt Worte, in denen Nummern beinhaltet sind. """
 def clean_text(text):
-    # Diese Funktion reduziert die Komplexität der Textpassagen.
-    # Sie entfernt Satzzeichen, setzt bei jedem Wort die Kleinschreibung durch und entfernt Worte, in denen Nummern beinhaltet sind
     text = text.lower()
     text = re.sub('\[.*?]', '', text)
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\w*\d\w*', '', text)
     return text
 
+
+"""Diese Funktion zerteilt den Text in einzelne Buchstaben und zählt die Häufigkeit der einzelnen Wörter.
+Die fünf Wörter, die am häufigsten erwähnt werden, sind das abschließende Ergebnis. """
 def tokenize_text(text):
-    # Diese Funktion zerteilt den Text in einzelne Buchstaben und zählt die Häufigkeit der einzenen Wörter.
-    # Die fünf Wörter, die am häufigsten erwähnt werden, sind das abschließende Ergebnis
+
     stop_words = set(stopwords.words('english'))
     word_tokens = word_tokenize(text)
     filtered_text = [w for w in word_tokens if not w.lower() in stop_words]
@@ -32,65 +33,67 @@ def tokenize_text(text):
             return frequent_words.most_common(5)
 
 
-# Die Funktion verbindet die beiden voranggegangenen Funktionen und zeigt die 5 häufigsten Wörter für jeden einzenen Text auf,
-# ohne beispielsweise Satzzeichen zu berücksichtigen
-# with open("Quellen.csv", "r") as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=";")
-#     skip_header = True
-#     i = 0
-#     for row in csv_reader:
-#         if skip_header:
-#             skip_header = False
-#             continue
-#         if row[0] == "ECOSIO":
-#             result = tokenize_text(clean_text(row[3]))
-#             print('Ecosio, ' + str(row[2]) + ': ' + str(result))
-#         if row[0] == "GEFEG":
-#             result= tokenize_text(clean_text(row[3]))
-#             print('GEFEG, ' + str(row[2]) + ': ' + str(result))
-#         i += 1
+"""Die Funktion verbindet die beiden voranggegangenen Funktionen und zeigt die fünf häufigsten Wörter für jeden einzelnen Text auf,
+ohne beispielsweise Satzzeichen als Ergebnis bei der Zählung zu berücksichtigen"""
+with open("Quellen.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=";")
+    skip_header = True
+    i = 0
+    for row in csv_reader:
+        if skip_header:
+            skip_header = False
+            continue
+        if row[0] == "ECOSIO":
+            result = tokenize_text(clean_text(row[3]))
+            print('Ecosio, ' + str(row[2]) + ': ' + str(result))
+        if row[0] == "GEFEG":
+            result= tokenize_text(clean_text(row[3]))
+            print('GEFEG, ' + str(row[2]) + ': ' + str(result))
+        i += 1
 
-# # Count Words
-# with open("Quellen.csv", "r") as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=";")
-#     skip_header = True
-#     i = 0
-#     for row in csv_reader:
-#         if skip_header:
-#             skip_header = False
-#             continue
-#         if row[0] == "ECOSIO":
-#             word_list = row[3].split()
-#             result = number_of_words = len(word_list)
-#             print('Ecosio, ' + str(row[2]) + ': ' + str(result))
-#         if row[0] == "GEFEG":
-#             word_list = row[3].split()
-#             result = number_of_words = len(word_list)
-#             print('GEFEG, ' + str(row[2]) + ': ' + str(result))
-#         i += 1
-
-
-# Diese Funktion berechnet die Tonalität (Sentiment) der jeweiligen
-# with open("Quellen.csv", "r") as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=";")
-#     skip_header = True
-#     i = 0
-#     for row in csv_reader:
-#         if skip_header:
-#             skip_header = False
-#             continue
-#         if row[0] == "ECOSIO":
-#             sia = SentimentIntensityAnalyzer()
-#             result = sia.polarity_scores(row[3])
-#             print('Ecosio, ' + str(row[2]) + ': ' + str(result))
-#         if row[0] == "GEFEG":
-#             sia = SentimentIntensityAnalyzer()
-#             result = sia.polarity_scores(row[3])
-#             print('GEFEG, ' + str(row[2]) + ': ' + str(result))
-#         i += 1
+"""Eine Funktion, welche die Worte in jedem Text zählt."""
+with open("Quellen.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=";")
+    skip_header = True
+    i = 0
+    for row in csv_reader:
+        if skip_header:
+            skip_header = False
+            continue
+        if row[0] == "ECOSIO":
+            word_list = row[3].split()
+            result = number_of_words = len(word_list)
+            print('Ecosio, ' + str(row[2]) + ': ' + str(result))
+        if row[0] == "GEFEG":
+            word_list = row[3].split()
+            result = number_of_words = len(word_list)
+            print('GEFEG, ' + str(row[2]) + ': ' + str(result))
+        i += 1
 
 
-# Diese Funktion berechnet den flesh Reading ease Score für jeden einzelnen Artikel
+"""Diese Funktion berechnet die Tonalität (Sentiment) der jeweiligen Texte. 
+Bei dieser Funktion wurden die Füllwörter nicht entfernt. 
+Versuche mit Änderungen des Programmcodes ein aussagekräftigeres Ergebnis zu erhalten haben keine besseren Ergebnisse erzielt."""
+with open("Quellen.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=";")
+    skip_header = True
+    i = 0
+    for row in csv_reader:
+        if skip_header:
+            skip_header = False
+            continue
+        if row[0] == "ECOSIO":
+            sia = SentimentIntensityAnalyzer()
+            result = sia.polarity_scores(row[3])
+            print('Ecosio, ' + str(row[2]) + ': ' + str(result))
+        if row[0] == "GEFEG":
+            sia = SentimentIntensityAnalyzer()
+            result = sia.polarity_scores(row[3])
+            print('GEFEG, ' + str(row[2]) + ': ' + str(result))
+        i += 1
+
+
+"""Diese Funktion berechnet den flesh Reading ease Score für jeden einzelnen Artikel."""
 with open("Quellen.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
     skip_header = True
